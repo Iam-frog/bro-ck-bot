c09ea6ee3477798030677c2ebb2d0b7d1422ca9c
Iam-frog <dcare7276@gmail.com>
2025-05-07 21:59:38 +0600

    Add files via upload

--- replit.nix
+++ replit.nix
@@ -1,11 +1,3 @@
-{ pkgs }: {
-	deps = [
-		pkgs.nodejs-16_x
-		pkgs.nodePackages.typescript-language-server
-		pkgs.libuuid
-		pkgs.replitPackages.jest
-	];
-	env = {
-		LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
-	};
+{pkgs}: {
+  deps = [ ];
 }
